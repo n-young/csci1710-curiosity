0634208e99a3dfdc75274597f0e278d9e5a6942c
Jed Fox <git@jedfox.com>
2021-02-13 14:04:47 -0500

    Update kenken.rkt

--- kenken.rkt
+++ kenken.rkt
@@ -4,14 +4,13 @@
 -- SIGNATURES
 -- ====================================================================
 
-sig Idx {
-    next: lone Idx
-    prev: lone Idx
+abstract sig Idx {
+    neighbor: lone Idx
 }
 one sig I1, I2, I3, I4 extends Idx {}
 
 pred isWellFormedIdx {
-    
+    neighbor = I1->I2 + I2->I3 + I3->I4
 }
 
 
@@ -20,14 +19,14 @@ sig Board {
 }
 
 sig Solution {
-    board: one Board
+    board: one Board,
     values: set Idx->Idx->Int
 }
 
 sig Cage {
-    operation: one Operation
-    cells: set Idx->Idx
-    result: Int
+    operation: one Operation,
+    cells: set Idx->Idx,
+    result: one Int
 }
 
 abstract sig Operation {}
@@ -40,21 +39,22 @@ one sig Division extends Operation {}
 -- PREDICATES
 -- ====================================================================
 
-pred isWellFormedSolution[Solution] -- assert all cells have a value in [1, n]
-pred isSolvedSolution[Solution] -- assert all cells are satisfied
-pred isWellFormedBoard[Board] -- assert all cages are valid, all cells are in exactly one cage
-pred isWellFormedCage[Cage] { -- assert all cells are adjacent; if sub or div, max 2 cells
-   all c: Cell |
-       c in Cage.cells |
-           some a:Cell |
-               
+pred isWellFormedSolution[soln: Solution] {} -- assert all cells have a value in [1, n]
+pred isSolvedSolution[soln: Solution] {} -- assert all cells are satisfied
+pred isWellFormedBoard[board: Board] {} -- assert all cages are valid, all cells are in exactly one cage
+pred isWellFormedCage[cage: Cage] { -- assert all cells are adjacent; if sub or div, max 2 cells
+some cage.cells
+all row: Idx | all col: Idx | row->col in cage.cells implies {
+    some Cage.cells & (row->col.neighbor + row->neighbor.col + row.neighbor->col + neighbor.row->col)
 }
-pred isSolvedCage[Cage, Solution] -- assert evaluateCage[Cage, Solution] == result
+cage.operation in Subtraction + Division implies #(cage.cells) = 2
+}
+pred isSolvedCage[cage: Cage, soln: Solution] {} -- assert evaluateCage[Cage, Solution] == result
+
+run { isWellFormedIdx all c: Cage | isWellFormedCage[c] } for exactly 1 Cage, 0 Solution, 0 Board
 
 -- ====================================================================
 -- TESTS
 -- ====================================================================
 
-pred isWell
-
-fun evaluateCage[Cage, Solution] -> Int
+-- fun evaluateCage[Cage, Solution] -> Int
