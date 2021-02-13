c8b0d81e8c753422fe2a381d8eb83d80f973a3fb
Nick Young <youngnicholas228@gmail.com>
2021-02-13 13:44:47 -0500

    a

--- kenken.rkt
+++ kenken.rkt
@@ -1,16 +1,27 @@
 #lang forge
 
-sig Idx {}
-one sig I1, I2, I3 extends Idx {}
--- add more later
+-- ====================================================================
+-- SIGNATURES
+-- ====================================================================
+
+sig Idx {
+    next: lone Idx
+    prev: lone Idx
+}
+one sig I1, I2, I3, I4 extends Idx {}
+
+pred isWellFormedIdx {
+    
+}
+
 
 sig Board {
-     cages: set Cage
+    cages: set Cage
 }
 
 sig Solution {
-     board: one Board
-     values: set Idx->Idx->Int -- row -> col -> number
+    board: one Board
+    values: set Idx->Idx->Int
 }
 
 sig Cage {
@@ -25,10 +36,25 @@ one sig Subtraction extends Operation {}
 one sig Multiplication extends Operation {}
 one sig Division extends Operation {}
 
+-- ====================================================================
+-- PREDICATES
+-- ====================================================================
+
 pred isWellFormedSolution[Solution] -- assert all cells have a value in [1, n]
 pred isSolvedSolution[Solution] -- assert all cells are satisfied
 pred isWellFormedBoard[Board] -- assert all cages are valid, all cells are in exactly one cage
-pred isWellFormedCage[Cage] -- assert all cells are adjacent; if sub or div, max 2 cells
+pred isWellFormedCage[Cage] { -- assert all cells are adjacent; if sub or div, max 2 cells
+   all c: Cell |
+       c in Cage.cells |
+           some a:Cell |
+               
+}
 pred isSolvedCage[Cage, Solution] -- assert evaluateCage[Cage, Solution] == result
+
+-- ====================================================================
+-- TESTS
+-- ====================================================================
+
+pred isWell
 
 fun evaluateCage[Cage, Solution] -> Int
