04c95a8367deb52da3a54f7880444e66d8243c25
Nick Young <youngnicholas228@gmail.com>
2021-02-13 15:22:20 -0500

    wellformedsol

--- kenken.rkt
+++ kenken.rkt
@@ -40,7 +40,15 @@ one sig Division extends Operation {}
 -- ====================================================================
 
 pred isWellFormedSolution[soln: Solution] {
+    -- Board is well formed.
+    isWellFormedBoard[soln.board]
+    -- Board cells equal to the solution cells TODO: Get rid of int column
+    soln.board.cages.cells = soln.values
     -- assert all cells have a value in [1, n]
+    all row: Idx | #(soln.values.row.Idx) = #(Idx)
+    all col: Idx | #(soln.values.Idx.col) = #(Idx)
+    all row: Idx | all col:Idx |
+        sol.values.row.col <= #(Idx) and sol.values.row.col > 0
 }
 
 pred isSolvedSolution[soln: Solution] {
@@ -62,67 +70,88 @@ pred isWellFormedCage[cage: Cage] {
     all row: Idx | all col: Idx | row->col in cage.cells implies {
         some cage.cells & (row->(col.neighbor) + row->(neighbor.col) + (row.neighbor)->col + (neighbor.row)->col)
     }
+    -- If subtraction or division, must be size 2
     cage.operation in Subtraction + Division implies #(cage.cells) = 2
+    -- If a singleton, must be addition
+    #(cage.cells) = 1 implies cage.operation in Addition
 }
 
 pred isSolvedCage[cage: Cage, soln: Solution] {
     -- assert evaluateCage[Cage, Solution] == result
 }
+
+
+
+-- ====================================================================
+-- TESTS (isWellFormedSolution)
+-- ====================================================================
+
+example StandardSolution is {all soln: Solution | isWellFormedSolution[solution]} for {
+    ...
+}
+
 -- ====================================================================
 -- TESTS (isWellFormedCage)
 -- ====================================================================
 
 -- Test on a 1x2 cage.
-inst StandardCage1Inst {
+example StandardCage1 is {all cage: Cage | isWellFormedCage[cage]} for {
     neighbor = I10->I20 + I20->I30 + I30->I40
     Cage = Cage0
     operation = Cage0->Multiplication0
     cells = Cage0->I10->I10 + Cage0->I20->I10
-    result = Cage0->7
+    result = Cage0->sing[7]
 }
-example StandardCage1 is {all cage: Cage | isWellFormedCage[cage]} for StandardCage1Inst
+
+-- Test on a 1x2 cage.
+example StandardCage1 is {all cage: Cage | isWellFormedCage[cage]} for {
+    neighbor = I10->I20 + I20->I30 + I30->I40
+    Cage = Cage0
+    operation = Cage0->Multiplication0
+    cells = Cage0->I10->I10 + Cage0->I20->I10
+    result = Cage0->sing[7]
+}
 
 -- Test on a 4-square cage.
-inst StandardCage2Inst {
+example StandardCage2 is {all cage: Cage | isWellFormedCage[cage]} for {
     neighbor = I10->I20 + I20->I30 + I30->I40
     Cage = Cage0
     operation = Cage0->Multiplication0
     cells = Cage0->I10->I10 + Cage0->I20->I10 + Cage0->I10->I20 + Cage0->I10->I30
-    result = Cage0->7
+    result = Cage0->sing[7]
 }
-example StandardCage2 is {all cage: Cage | isWellFormedCage[cage]} for StandardCage2Inst
 
 -- Test on a disconnected cage (1,1) + (2, 2)
-inst DisconnectedInst {
+example Disconnected is {some cage: Cage | not isWellFormedCage[cage]} for {
     neighbor = I10->I20 + I20->I30 + I30->I40
     Cage = Cage0
     operation = Cage0->Multiplication0
     cells = Cage0->I10->I10 + Cage0->I20->I20
-    result = Cage0->7
+    result = Cage0->sing[7]
 }
-example Disconnected is {some cage: Cage | not isWellFormedCage[cage]} for DisconnectedInst
 
 -- Test that subtraction can't be too big
-inst BigSubtractionInst {
+example SubtractionBig is {some cage: Cage | not isWellFormedCage[cage]} for {
     neighbor = I10->I20 + I20->I30 + I30->I40
     Cage = Cage0
     operation = Cage0->Subtraction0
     cells = Cage0->I10->I10 + Cage0->I20->I10 + Cage0->I10->I20
-    result = Cage0->7
+    result = Cage0->sing[7]
 }
-example Disconnected is {some cage: Cage | not isWellFormedCage[cage]} for DisconnectedInst
 
 -- Test that division can't be too big
-inst BigDivisionInst {
+example DivisionBig is {some cage: Cage | not isWellFormedCage[cage]} for {
     neighbor = I10->I20 + I20->I30 + I30->I40
     Cage = Cage0
     operation = Cage0->Division0
     cells = Cage0->I10->I10 + Cage0->I20->I10 + Cage0->I10->I20
-    result = Cage0->7
+    result = Cage0->sing[7]
 }
-example Disconnected is {some cage: Cage | not isWellFormedCage[cage]} for DisconnectedInst
 
 
+-- ====================================================================
+-- RUN
+-- ====================================================================
 
 run {
     isWellFormedIdx
