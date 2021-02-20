33809818b638ca9ad79a7a8a63b81480ec8282bb
Jed Fox <git@jedfox.com>
2021-02-20 11:59:34 -0500

    Update cage.js

--- cage.js
+++ cage.js
@@ -1,5 +1,6 @@
 const indexes = [I1, I2, I3, I4]
-const idxToInt = (idx) => indexes.findIndex((i) => i.equals(idx))
+const idxToNumber = idx => indexes.findIndex(i => i.equals(idx))
+const intToNumber = int => parseInt(int.toString())
 const labels = new Map([
   [Addition0, '+'],
   [Subtraction0, '-'],
@@ -15,17 +16,32 @@ const cageArray = Array(indexes.length)
 Cage.atoms().forEach((cage, i) => {
   for (const cell of cage.join(cells).tuples()) {
     const [row, col] = cell.atoms()
-    const rowIdx = idxToInt(row),
-      colIdx = idxToInt(col)
+    const rowIdx = idxToNumber(row),
+      colIdx = idxToNumber(col)
     if (cageArray[rowIdx][colIdx] != null) {
-      throw new Error(`Dupe at ${rowIdx + 1}, ${colIdx + 1}: Cage${i} and Cage${cageArray[rowIdx][colIdx].i}`)
+      throw new Error(
+        `Dupe at ${rowIdx + 1}, ${colIdx + 1}: Cage${i} and Cage${
+          cageArray[rowIdx][colIdx].i
+        }`
+      )
     }
-    debugger
-    cageArray[rowIdx][colIdx] = { i, op: labels.get(unwrap(cage.join(operation))) }
+    cageArray[rowIdx][colIdx] = {
+      i,
+      op: labels.get(unwrap(cage.join(operation))),
+    }
   }
 })
 const table = document.createElement('table')
-const colors = ['#B10DC9', '#0074D9', '#2ECC40', '#85144b', '#39CCCC', '#FF851B', '#FF4136', '#AAAAAA']
+const colors = [
+  '#B10DC9',
+  '#0074D9',
+  '#2ECC40',
+  '#85144b',
+  '#39CCCC',
+  '#FF851B',
+  '#FF4136',
+  '#AAAAAA',
+]
 for (const row of cageArray) {
   const tr = document.createElement('tr')
   table.appendChild(tr)
@@ -43,8 +59,41 @@ for (const row of cageArray) {
 }
 table.style.border = '1px solid black'
 table.style.margin = '1em'
+
+const solutionValues = Array(indexes.length)
+  .fill()
+  .map(() => Array(indexes.length).fill())
+for (const cell of Solution0.join(values).tuples()) {
+  const [row, col, value] = cell.atoms()
+  const rowIdx = idxToNumber(row),
+    colIdx = idxToNumber(col)
+  if (solutionValues[rowIdx][colIdx] != null) {
+    throw new Error(
+      `Dupe at ${rowIdx + 1}, ${colIdx + 1}: ${value} and ${
+        solutionValues[rowIdx][colIdx]
+      }`
+    )
+  }
+  solutionValues[rowIdx][colIdx] = intToNumber(value)
+}
+const table2 = document.createElement('table')
+for (const row of solutionValues) {
+  const tr = document.createElement('tr')
+  table2.appendChild(tr)
+  for (const col of row) {
+    const td = document.createElement('td')
+    tr.appendChild(td)
+    td.textContent = col
+    td.style.textAlign = 'center'
+    td.style.width = '22px'
+    td.style.height = '2px'
+    td.style.border = '1px solid black'
+  }
+}
+table2.style.margin = '1em'
+table2.style.borderCollapse = 'collapse'
+
 div.innerHTML = ''
 div.appendChild(table)
-
-
+div.appendChild(table2)
 
